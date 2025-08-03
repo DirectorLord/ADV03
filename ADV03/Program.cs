7066eb97fe7ff551cd897c83f45e3281cb5b7af9
DirectorLord <thelord161203@gmail.com>
2025-08-03 15:22:50 +0300

    solved question 1

--- ADV03/Program.cs
+++ ADV03/Program.cs
@@ -1,8 +1,57 @@
 ﻿namespace ADV03;
 public class Program
 {
+    #region Question1
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string isbn, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            this.ISBN = isbn;
+            this.Title = title;
+            this.Authors = authors;
+            this.PublicationDate = publicationDate;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {string.Join(", ", Authors)}, Publication Date: {PublicationDate}, Price: {Price:C}";
+        }
+    }
+
+    public class BookFunctions
+    {
+        public static string GetTitle(Book b)
+        {
+            return b.Title;
+        }
+
+        public static string GetAuthors(Book b)
+        {
+            return string.Join(", ", b.Authors);
+        }
+
+        public static string GetPublicationDate(Book b)
+        {
+            return b.PublicationDate.ToShortDateString();
+        }
+
+        public static string GetPrice(Book b)
+        {
+            return b.Price.ToString("C");
+        }
+    }
+    #endregion
+
+
     public static void Main(String[] args)
     {
-       
+
     }
 }
